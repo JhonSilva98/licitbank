6daa7cb8fe87e0d6a802e8a156cce9ea7b478955
JhonSilva98 <jhonatansilva98@outlook.com>
2024-01-21 23:03:17 -0300

    insercao seachbar

--- lib/widget/telaPrincipal/telaprincipal.dart
+++ lib/widget/telaPrincipal/telaprincipal.dart
@@ -11,7 +11,32 @@ class _TelaprincipalState extends State<Telaprincipal> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu_outlined,
+          ),
+        ),
+        title: const Text("LicitBank"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.person_2_rounded),
+          )
+        ],
+      ),
+      body: const Center(
+        child: Column(
+          children: [
+            SizedBox(
+              child: SearchBar(),
+            ),
+            SizedBox(),
+          ],
+        ),
+      ),
     );
   }
 }
